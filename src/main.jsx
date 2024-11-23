3d2556cdc658e337a6fd5f028b9678f81c1990cf
PillowGit <goldentaco727@gmail.com>
2024-11-23 11:40:31 -0800

    Started project basics

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+// Do not touch
+// This file is the entry point for React
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
