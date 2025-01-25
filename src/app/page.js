0ccd92c9d00621b6ac25cde2634d941b76f50151
PillowGit <goldentaco727@gmail.com>
2025-01-25 12:34:56 -0800

    Next.js fully added

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,9 @@ import { motion } from "motion/react";
 export default function Home() {
   return <>
     <title>GitGood</title>
-    <div className="w-screen min-h-screen bg-[#131313] absolute left-0 top-0 flex justify-center items-center">
-      <p className="font-[#F7F7F2] font-bold text-3xl">GitGood.cc soon</p>
+    <div className="w-screen min-h-screen bg-[#131313] absolute left-0 top-0 flex justify-center items-center flex-col">
+      <p className="font-[#F7F7F2] font-bold text-3xl">GitGood.cc in development</p>
+      <a href="https://github.com/PillowGit/gitgood" className="font-[#F7F7F2] font-bold text-3xl mt-8 underline transition hover:font-[#C4C4C2] hover:scale-105">github.com/PillowGit/gitgood</a>
     </div>
   </>;
 }
