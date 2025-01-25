6c7b512d65e79b6c1dc4315b84beb6b14e96d26e
PillowGit <goldentaco727@gmail.com>
2025-01-25 12:10:54 -0800

    Remove old framework (in prep for adding next.js)

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="w-full h-screen flex align-middle justify-center">
-      <h1 className="text-3xl font-bold underline">gitgood coming soon!</h1>
-    </div>
-  );
-}
