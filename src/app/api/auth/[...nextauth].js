23c840b530ba1a65583de1ece472fbdf0a70dd2d
HallowsYves <83028938+HallowsYves@users.noreply.github.com>
2025-02-03 20:12:47 -0800

    Fixed minor bugs and issues
    Fixed syntax error in next.config.mjs
    Adjusted syntax for GitHubProvider
    Created Providers.jsx for the client component
    
    Setup Layout.js

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
-import GithubProvider from 'next-auth/providers';
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions = {
     providers: [
-        GithubProvider({
+        GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
