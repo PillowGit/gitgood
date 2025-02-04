b1100faef4472a07df0b6984bf2c19c78d94cd06
HallowsYves <83028938+HallowsYves@users.noreply.github.com>
2025-02-03 21:04:17 -0800

    Almost done

---
+++ src/app/protected/page.js
@@ -0,0 +1,22 @@
+"use client"; 
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Protected() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div>
+        <img src="protected.jpg" width={500} height={500} />
+        <button onClick={() => signOut()}>Sign Out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => signIn()}>Sign In</button>
+    </div>
+  );
+}
