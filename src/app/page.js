81090de3b21f489d5e3eba5c4255c2a0f84b80db
PillowGit <goldentaco727@gmail.com>
2025-02-03 23:12:29 -0800

    Fixed weird centering error mb

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return <>
     <title>GitGood</title>
-    <div className="min-w-screen min-h-screen bg-[#131313] absolute left-0 top-0 flex justify-center items-center flex-col">
+    <div className="w-screen min-h-screen bg-[#131313] absolute left-0 top-0 flex justify-center items-center flex-col">
       <p className="font-[#F7F7F2] font-bold text-3xl">GitGood.cc in development</p>
       <a href="https://github.com/PillowGit/gitgood" className="font-[#F7F7F2] font-bold text-3xl mt-8 underline transition hover:font-[#C4C4C2] hover:scale-105">github.com/PillowGit/gitgood</a>
       <Link href="/protected"
