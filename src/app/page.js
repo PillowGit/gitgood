66c618cff9ee81e3a2e2ccfb92aeec26a3a9f334
PillowGit <goldentaco727@gmail.com>
2025-02-03 23:04:59 -0800

    Small styling changes for auth demo

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
-import { motion } from "motion/react";
 import Link from 'next/link';
 
 
 export default function Home() {
   return <>
     <title>GitGood</title>
-    <div className="w-screen min-h-screen bg-[#131313] absolute left-0 top-0 flex justify-center items-center flex-col">
+    <div className="min-w-screen min-h-screen bg-[#131313] absolute left-0 top-0 flex justify-center items-center flex-col">
       <p className="font-[#F7F7F2] font-bold text-3xl">GitGood.cc in development</p>
-      <Link href={"/protected"}> PRESS HERE</Link>
       <a href="https://github.com/PillowGit/gitgood" className="font-[#F7F7F2] font-bold text-3xl mt-8 underline transition hover:font-[#C4C4C2] hover:scale-105">github.com/PillowGit/gitgood</a>
+      <Link href="/protected"
+      className="mt-8 border-2 border-[#4e4e4ea4] rounded-lg transition hover:scale-110 hover:bg-[#4e4e4ea4] hover:border-[#4e4e4ea4] hover:text-[#F7F7F2] cursor-pointer"
+      >
+        <div className="my-2 mx-4">View Login Page</div>
+      </Link>
     </div>
-
   </>;
 }
