b98d7f6f5444d5c82ec49219e0c1f77157e21c8d
PillowGit <goldentaco727@gmail.com>
2025-02-14 17:03:05 -0800

    Fixed security issue with all logged in users being able to see all other users private data

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -46,9 +46,7 @@ export async function GET(req, { params }) {
 
   // Check if user is logged in
   if (github_id) {
-    // If they are logged in but their data doesn't exist, create it
-    // Otherwise, if they requested an invalid user, return error
-    // Finally, if all is right, they are authorized to see all their data
+    // If the user doesn't exist but is logged in, and wants their data, create them
     if (user.error && userId === github_id) {
       // Request their username from Github
       const githubUserData = await fetch(
@@ -62,17 +60,47 @@ export async function GET(req, { params }) {
         session.user.image
       );
       return NextResponse.json(userData);
+    // If the user doesn't exist and the person logged in is not them, return error
     } else if (user.error) {
       return NextResponse.json(
         { error: "User does not exist" },
         { status: 404 }
       );
-    } else {
+    // If the user exists and is logged in, update their data
+    } else if (userId === github_id) {
       let updated = await SyncGithubUsername(userId, session.user.name);
       if (updated) {
         user.username = session.user.name;
       }
       updated = await SyncGithubDisplayName(userId, session.user.name);
+      if (updated) {
+        user.display_name = session.user.name;
+      }
+      return NextResponse.json(user);
+    // If the user exists and is logged in, but is not them, only show public info
+    } else {
+      // Delete fields based off settings
+      if (!user.ownership_is_public) {
+        delete user.created;
+      }
+      if (!user.points_are_public) {
+        delete user.points_accumulated;
+      }
+      if (!user.accepted_are_public) {
+        delete user.accepted;
+      }
+      // Never used publicly
+      delete user.attempted;
+      delete user.can_create;
+      // Settings are not public
+      delete user.ownership_is_public;
+      delete user.points_are_public;
+      delete user.accepted_are_public;
+      let updated = await SyncGithubUsername(userId, user.username);
+      if (updated) {
+        user.username = session.user.name;
+      }
+      updated = await SyncGithubDisplayName(userId, user.display_name);
       if (updated) {
         user.display_name = session.user.name;
       }
