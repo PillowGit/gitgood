0b59c15f4b0280c4843cb6629227047e12198d75
PillowGit <goldentaco727@gmail.com>
2025-02-14 16:07:55 -0800

    Test function for that

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,17 @@
+"use client";
+
 import Link from "next/link";
 
 export default function Home() {
+  async function test() {
+    const id = "104609738";
+    fetch(`/api/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        alert(JSON.stringify(data));
+      });
+  }
+
   return (
     <>
       <title>GitGood</title>
@@ -20,6 +31,16 @@ export default function Home() {
         >
           <div className="my-2 mx-4">View Login Page</div>
         </Link>
+
+        <div
+          className="mt-8 border-2 border-[#4e4e4ea4] rounded-lg transition hover:scale-110 hover:bg-[#4e4e4ea4] hover:border-[#4e4e4ea4] hover:text-[#F7F7F2] cursor-pointer"
+          role="button"
+          tabIndex="0"
+          aria-pressed="false"
+          onClick={test}
+        >
+          <h3 className="my-2 mx-4">Test Shit</h3>
+        </div>
       </div>
     </>
   );
