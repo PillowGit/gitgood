639751da895dc52dc6d0cda23b82db9e28ebf5d9
KQPH <82293933+KQPH@users.noreply.github.com.>
2025-02-21 20:50:33 -0800

    Added comment to docs API

--- src/app/api/docs/route.js
+++ src/app/api/docs/route.js
@@ -1,6 +1,20 @@
 import { NextResponse } from "next/server";
 import openapiSpecification from "../../../../swagger";
 
+/**
+ * @openapi
+ * /api/docs:
+ *  get:
+ *    description: Retrieve OpenAPI specification.
+ *    responses:
+ *      200:
+ *        description: Successfully retrieved OpenAPI specification.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              description: The OpenAPI specification.
+ */
 export function GET() {
   return NextResponse.json(openapiSpecification);
 }
