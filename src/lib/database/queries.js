eaaa78856751c383fbedb4d69b66368aa8686a3f
PillowGit <goldentaco727@gmail.com>
2025-02-26 15:45:15 -0800

    Fix firebase function logic for queries based on new query type definitions

--- src/lib/database/queries.js
+++ src/lib/database/queries.js
@@ -1,5 +1,6 @@
 import { db } from "@/lib/database/firebase";
 import { deepCopy } from "@/lib/utilities";
+import { getUser } from "@/lib/database/users";
 import {
   collection,
   doc,
@@ -44,16 +45,14 @@ const default_tags = {
 };
 /** @type {QueryOptions} */
 const default_query_options = {
-  tags: deepCopy(default_tags),
+  filter_tag: "",
   order_by: "",
-  order_asc: false,
   limit: 10,
   start_after: "",
   start_at: "",
   difficulty: "",
   difficulty_range: "",
   author: "",
-  language: "",
 };
 
 /**
@@ -76,33 +75,15 @@ function validateQueryOptions(options) {
       return { status: false, reason: `Missing key: ${key}` };
     }
   }
-  // Ensure all tags in the query options
-  for (const key of Object.keys(default_query_options.tags)) {
-    if (options.tags[key] === undefined) {
-      return { status: false, reason: `Missing tag: ${key}` };
-    }
-  }
   // Ensure no unknown keys are present
   for (const key of Object.keys(options)) {
     if (default_query_options[key] === undefined) {
       return { status: false, reason: `Unknown key: ${key}` };
     }
   }
-  // Ensure no unknown tags are present
-  for (const key of Object.keys(options.tags)) {
-    if (default_query_options.tags[key] === undefined) {
-      return { status: false, reason: `Unknown tag: ${key}` };
-    }
-  }
-  // Ensure only one or zero tags are present
-  const sort_tags = Object.keys(options.tags).filter(
-    (tag) => options.tags[tag]
-  );
-  if (sort_tags.length > 1) {
-    return {
-      status: false,
-      reason: "Cannot filter by more than one tag at a time",
-    };
+  // Ensure given tag is valid
+  if (options.filter_tag !== "" && !default_tags[options.filter_tag]) {
+    return { status: false, reason: `Invalid tag: ${options.filter_tag}` };
   }
   // Ensure order_by is valid ("", "difficulty", "votes", "updated", "created")
   if (
@@ -127,14 +108,18 @@ function validateQueryOptions(options) {
     };
   }
   // Ensure difficulty_range is valid ("" or "0.0-10.0")
-  if (
-    options.difficulty_range !== "" &&
-    !options.difficulty_range.match(/^(\d\.\d)-(\d\.\d)$/)
-  ) {
-    return {
-      status: false,
-      reason: `Invalid difficulty_range: ${options.difficulty_range}`,
-    };
+  if (difficulty === "" && options.difficulty_range !== "") {
+    if (
+      !options.difficulty_range.match(/^(\d\.\d)-(\d\.\d)$/) ||
+      options.difficulty_range
+        .split("-")
+        .some((num) => Number(num) < 0.0 || Number(num) > 10.0)
+    ) {
+      return {
+        status: false,
+        reason: `Invalid difficulty_range: ${options.difficulty_range}`,
+      };
+    }
   }
   // Ensure limit is valid (1-20)
   if (options.limit < 1 || options.limit > 20) {
@@ -162,91 +147,123 @@ async function queryQuestions(options) {
     if (!validation.status) {
       return { error: validation.reason };
     }
-    // Begin building the query by listing out the query parameters
-    const query_params = [];
-    query_params.push(collection(db, "questions-short"));
 
-    // Never show unlisted questions
-    query_params.push(where("display_publicly", "==", true));
-    // Filter by language
-    if (options.language !== "") {
-      query_params.push(where("languages", "array-contains", options.language));
-    }
-    // Filter by author
-    if (options.author !== "") {
-      query_params.push(where("author_id", "==", options.author));
-    }
-    // Filter by difficulty first
-    if (options.difficulty !== "") {
-      const lower =
-        options.difficulty === "easy"
-          ? 0.0
-          : options.difficulty === "medium"
-          ? 3.3
-          : 6.7;
-      const upper =
-        options.difficulty === "easy"
-          ? 3.2
-          : options.difficulty === "medium"
-          ? 6.6
-          : 10.0;
-      query_params.push(where("difficulty", ">=", lower));
-      query_params.push(where("difficulty", "<=", upper));
-      // Filter by range if no difficulty and range filled out
-    } else if (options.difficulty_range !== "") {
-      const range = options.difficulty_range.split("-");
-      query_params.push(where("difficulty", ">=", range[0]));
-      query_params.push(where("difficulty", "<=", range[1]));
-    }
-    // Filter by tags
-    const sort_tags = Object.keys(options.tags).filter(
-      (tag) => options.tags[tag]
-    );
-    if (sort_tags.length > 1) {
-      return { error: "Cannot filter by more than one tag at a time" };
-    } else if (sort_tags.length === 1) {
-      query_params.push(where(`tags.${sort_tags[0]}`, "==", true));
-    }
+    if (author !== "") {
+      // Make sure the requested author actually exists
+      const user = await getUser(author);
+      if (user.error) {
+        return {
+          error: `Author ID given during query, but failed to fetch Author due to: ${user.error}`,
+        };
+      }
 
-    // Order by
-    let ordering = "votes_sum";
-    if (options.order_by === "difficulty") {
-      ordering = "difficulty";
-    } else if (options.order_by === "updated") {
-      ordering = "date_updated";
-    } else if (options.order_by === "created") {
-      ordering = "date_created";
-    }
-    if (options.order_asc) {
-      query_params.push(orderBy(ordering));
+      // Start building parameters for the query
+      const query_params = [];
+      query_params.push(collection(db, "questions-short"));
+
+      // Filter by author
+      query_params.push(where("author_id", "==", author));
+
+      // Allow cursoring through the results
+      if (options.start_after !== "") {
+        const questRef = collection(db, "questions-short");
+        const cursorSnap = await getDoc(doc(questRef, options.start_after));
+        query_params.push(startAfter(cursorSnap));
+      } else if (options.start_at !== "") {
+        const questRef = collection(db, "questions-short");
+        const cursorSnap = await getDoc(doc(questRef, options.start_at));
+        query_params.push(startAt(cursorSnap));
+      }
+
+      // Use the given limit
+      query_params.push(limit(options.limit));
+
+      // Make query
+      const q = query(...query_params);
+      const querySnapshot = await getDocs(q);
+
+      // Return result
+      const metadata = [];
+      querySnapshot.forEach((queryDocSnap) => {
+        metadata.push(queryDocSnap.data());
+      });
+      return metadata;
     } else {
-      query_params.push(orderBy(ordering, "desc"));
-    }
-    // Limit
-    query_params.push(limit(options.limit));
-    // Start after
-    if (options.start_after !== "") {
-      const questRef = collection(db, "questions-short");
-      const cursorSnap = await getDoc(doc(questRef, options.start_after));
-      query_params.push(startAfter(cursorSnap));
-    }
-    // Start at
-    if (options.start_at !== "") {
-      const questRef = collection(db, "questions-short");
-      const cursorSnap = await getDoc(doc(questRef, options.start_at));
-      query_params.push(startAt(cursorSnap));
-    }
+      // Begin building the query by listing out the query parameters
+      const query_params = [];
+      query_params.push(collection(db, "questions-short"));
 
-    // Spread out the query params into a query object
-    let q = query(...query_params);
+      // Never show unlisted questions
+      query_params.push(where("display_publicly", "==", true));
 
-    // Get the documents
-    const querySnapshot = await getDocs(q);
-    const metadata = [];
-    querySnapshot.forEach((queryDocSnap) => {
-      metadata.push(queryDocSnap.data());
-    });
-    return metadata;
+      // Filter by difficulty first
+      if (options.difficulty !== "") {
+        const lower =
+          options.difficulty === "easy"
+            ? 0.0
+            : options.difficulty === "medium"
+            ? 3.3
+            : 6.7;
+        const upper =
+          options.difficulty === "easy"
+            ? 3.2
+            : options.difficulty === "medium"
+            ? 6.6
+            : 10.0;
+        query_params.push(where("difficulty", ">=", lower));
+        query_params.push(where("difficulty", "<=", upper));
+        // Filter by range if no difficulty and range filled out
+      } else if (options.difficulty_range !== "") {
+        const range = options.difficulty_range.split("-");
+        query_params.push(where("difficulty", ">=", range[0]));
+        query_params.push(where("difficulty", "<=", range[1]));
+      }
+
+      // Filter by tag if given
+      if (options.filter_tag !== "") {
+        query_params.push(where(`tags.${options.filter_tag}`, "==", true));
+      }
+
+      // Order by
+      let ordering = "votes_sum";
+      if (options.order_by === "difficulty") {
+        ordering = "difficulty";
+      } else if (options.order_by === "updated") {
+        ordering = "date_updated";
+      } else if (options.order_by === "created") {
+        ordering = "date_created";
+      }
+      if (options.order_asc) {
+        query_params.push(orderBy(ordering));
+      } else {
+        query_params.push(orderBy(ordering, "desc"));
+      }
+
+      // Limit
+      query_params.push(limit(options.limit));
+
+      // Cursors for paging
+      if (options.start_after !== "") {
+        const questRef = collection(db, "questions-short");
+        const cursorSnap = await getDoc(doc(questRef, options.start_after));
+        query_params.push(startAfter(cursorSnap));
+      } else if (options.start_at !== "") {
+        const questRef = collection(db, "questions-short");
+        const cursorSnap = await getDoc(doc(questRef, options.start_at));
+        query_params.push(startAt(cursorSnap));
+      }
+
+      // Spread out the query params into a query object and make query
+      let q = query(...query_params);
+      const querySnapshot = await getDocs(q);
+
+      // Get the documents
+      const metadata = [];
+      querySnapshot.forEach((queryDocSnap) => {
+        metadata.push(queryDocSnap.data());
+      });
+      return metadata;
+    }
   } catch (e) {
     console.log("Caught error while querying questions", e);
     return { error: e.message };
