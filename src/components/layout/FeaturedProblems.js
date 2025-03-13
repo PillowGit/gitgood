82589532174d82bd3186ab54e0cbfea7227fa456
PillowGit <goldentaco727@gmail.com>
2025-03-12 19:09:22 -0700

    Featured problems on homepage now actually load and do things

--- src/components/layout/FeaturedProblems.js
+++ src/components/layout/FeaturedProblems.js
@@ -1,5 +1,6 @@
 import { Card } from "@/components/ui/Card";
 import { getFeaturedQuestions } from "@/lib/proxies/featured";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function FeaturedProblems() {
@@ -36,23 +37,28 @@ export default function FeaturedProblems() {
       <h2 className="text-2xl mt-6 mb-4">Featured Problems</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {questions.map((problem, i) => (
-          <Card key={i} className="bg-[#282828] border-none">
-            <div className="p-0 bg-[#282828]">
-              <img
-                src={problem.image || "@/icon.svg"}
-                alt={problem.title}
-                width={300}
-                height={200}
-                className="h-fit w-fit rounded-lg"
-              />
-            </div>
-            <div className="bg-[#282828] pt-2">
-              <h3 className="text-xl font-medium">{problem.title}</h3>
-              <p className="text-sm text-gray-400">
-                Created by: {problem.author}
-              </p>
-            </div>
-          </Card>
+          <Link href={`/challenge/${problem.questionid}`} key={i}>
+            <Card
+              key={i}
+              className="bg-[#282828] border-none transition hover:scale-[103%] hover:shadow-lg focus:scale-[103%] focus:shadow-lg"
+            >
+              <div className="p-0 bg-[#282828]">
+                <img
+                  src={problem.image || "@/icon.svg"}
+                  alt={problem.title}
+                  width={300}
+                  height={200}
+                  className="h-fit w-fit rounded-lg"
+                />
+              </div>
+              <div className="bg-[#282828] pt-2">
+                <h3 className="text-xl font-medium">{problem.title}</h3>
+                <p className="text-sm text-gray-400">
+                  Created by: {problem.author}
+                </p>
+              </div>
+            </Card>
+          </Link>
         ))}
       </div>
     </section>
