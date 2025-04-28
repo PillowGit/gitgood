a55a7e367f3a3891267cf1c30433c9b88cef93ee
PillowGit <goldentaco727@gmail.com>
2025-04-28 12:23:09 -0700

    Clarify language versions for people creating questions

--- src/components/createlayout/RuntimeForm.jsx
+++ src/components/createlayout/RuntimeForm.jsx
@@ -2,6 +2,7 @@
 
 import Editor from "@monaco-editor/react";
 import { languageList } from "@/constants/languageList";
+import Link from "next/link";
 
 function RuntimeForm({
   codeLanguage,
@@ -14,17 +15,26 @@ function RuntimeForm({
   inputs,
   setInputs,
   tester,
-  setTester
+  setTester,
 }) {
   return (
     <div className="space-y-6">
       <div className="space-y-2">
         <label className="block text-sm">
           <strong>
-            This section handles your challenge's logic. You are responsible for writing
-            all the test case variables, solution function, template function, and the 
-            function that will run tests between the two. Your code will not be executed
-            and quality checked, so please ensure that it is correct.
+            This section handles your challenge's logic. You are responsible for
+            writing all the test case variables, solution function, template
+            function, and the function that will run tests between the two. Your
+            code will not be executed and quality checked, so please ensure that
+            it is correct. Please note that all the languages are restricted to
+            the versions listed here:{" "}
+            <Link
+              href="https://emkc.org/api/v2/piston/runtimes"
+              className="text-blue-500"
+            >
+              https://emkc.org/api/v2/piston/runtimes
+            </Link>
+            .
           </strong>
         </label>
         <label className="block text-sm">
@@ -34,11 +44,11 @@ function RuntimeForm({
         <select
           className="w-full bg-[#333333] border-none text-white p-3 rounded"
           value={codeLanguage}
-          onChange={e => setCodeLanguage(e.target.value)}
+          onChange={(e) => setCodeLanguage(e.target.value)}
         >
           <option value="c++">c++</option>
           <option value="python">python3</option>
-          {languageList.map(language => {
+          {languageList.map((language) => {
             return (
               <option key={language} value={language}>
                 {language}
@@ -50,10 +60,11 @@ function RuntimeForm({
 
       <div className="space-y-2">
         <label className="block text-sm">
-          Write down (hard code) all of the testcase variables in this function. Note that this
-          is the top of the file, so you should add any libraries you want here. Also
-          note that if you are using a language that requires a main function, you
-          will need to add that main function somewhere in the code.
+          Write down (hard code) all of the testcase variables in this function.
+          Note that this is the top of the file, so you should add any libraries
+          you want here. Also note that if you are using a language that
+          requires a main function, you will need to add that main function
+          somewhere in the code.
         </label>
         <div className="border border-gray-700 rounded-lg overflow-hidden shadow-lg">
           <Editor
@@ -65,7 +76,7 @@ function RuntimeForm({
             options={{
               fontSize: 14,
               minimap: { enabled: false },
-              automaticLayout: true
+              automaticLayout: true,
             }}
             className="w-full h-64" // fixed height for better consistency
           />
@@ -74,20 +85,23 @@ function RuntimeForm({
 
       <div className="space-y-2">
         <label className="block text-sm">
-          Write the template function that users must complete. Ensure all of the parameters are in the header.
-          The user will get this empty function, and it shoudl be clear for them to fill out and submit.
+          Write the template function that users must complete. Ensure all of
+          the parameters are in the header. The user will get this empty
+          function, and it shoudl be clear for them to fill out and submit.
         </label>
         <div className="border border-gray-700 rounded-lg overflow-hidden shadow-lg">
           <Editor
             id="codeTemplate"
             language={mapLanguage(codeLanguage)}
-            onChange={(value, event) => setCodeTemplate(value?.split("\n") || [])}
+            onChange={(value, event) =>
+              setCodeTemplate(value?.split("\n") || [])
+            }
             theme="vs-dark"
             defaultValue={codeTemplate.join("\n")}
             options={{
               fontSize: 14,
               minimap: { enabled: false },
-              automaticLayout: true
+              automaticLayout: true,
             }}
             className="w-full h-64" // fixed height for better consistency
           />
@@ -96,20 +110,22 @@ function RuntimeForm({
 
       <div className="space-y-2">
         <label className="block text-sm">
-          Write a solution function to your problem. This is the function that will be
-          compared against the user's template function.
+          Write a solution function to your problem. This is the function that
+          will be compared against the user's template function.
         </label>
         <div className="border border-gray-700 rounded-lg overflow-hidden shadow-lg">
           <Editor
             id="codeSolution"
             language={mapLanguage(codeLanguage)}
-            onChange={(value, event) => setCodeSolution(value?.split("\n") || [])}
+            onChange={(value, event) =>
+              setCodeSolution(value?.split("\n") || [])
+            }
             theme="vs-dark"
             defaultValue={codeSolution.join("\n")}
             options={{
               fontSize: 14,
               minimap: { enabled: false },
-              automaticLayout: true
+              automaticLayout: true,
             }}
             className="w-full h-64" // fixed height for better consistency
           />
@@ -134,7 +150,7 @@ function RuntimeForm({
             options={{
               fontSize: 14,
               minimap: { enabled: false },
-              automaticLayout: true
+              automaticLayout: true,
             }}
             className="w-full h-64" // fixed height for better consistency
           />
