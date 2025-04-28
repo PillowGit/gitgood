09fb75021515e0ac6fd7d83d013d464e106be981
PillowGit <goldentaco727@gmail.com>
2025-04-28 02:52:07 -0700

    Change wording in create page to be more clear

--- src/components/createlayout/RuntimeForm.jsx
+++ src/components/createlayout/RuntimeForm.jsx
@@ -21,11 +21,10 @@ function RuntimeForm({
       <div className="space-y-2">
         <label className="block text-sm">
           <strong>
-            This section handles your challenge's logic. You are responsible for
-            creating all of your test cases, parsing them, providing the template for the user,
-            and running the parsed test cases against your solution. Imagine all of these sections were combined to
-            create one large file. 
-
+            This section handles your challenge's logic. You are responsible for writing
+            all the test case variables, solution function, template function, and the 
+            function that will run tests between the two. Your code will not be executed
+            and quality checked, so please ensure that it is correct.
           </strong>
         </label>
         <label className="block text-sm">
@@ -51,8 +50,10 @@ function RuntimeForm({
 
       <div className="space-y-2">
         <label className="block text-sm">
-          Write down all of the testcases and ensure that that they can be
-          parsed in the function that you create at the very bottom.
+          Write down (hard code) all of the testcase variables in this function. Note that this
+          is the top of the file, so you should add any libraries you want here. Also
+          note that if you are using a language that requires a main function, you
+          will need to add that main function somewhere in the code.
         </label>
         <div className="border border-gray-700 rounded-lg overflow-hidden shadow-lg">
           <Editor
@@ -60,7 +61,7 @@ function RuntimeForm({
             language={mapLanguage(codeLanguage)}
             onChange={(value, event) => setInputs(value?.split("\n") || [])}
             theme="vs-dark"
-            defaultValue={`std::vector<int> testCases = {5, 10, 15, 20};`}
+            defaultValue={`#include <vector>\n#include <iostream>\nstd::vector<int> testCases = {5, 10, 15, 20};`}
             options={{
               fontSize: 14,
               minimap: { enabled: false },
@@ -74,6 +75,7 @@ function RuntimeForm({
       <div className="space-y-2">
         <label className="block text-sm">
           Write the template function that users must complete. Ensure all of the parameters are in the header.
+          The user will get this empty function, and it shoudl be clear for them to fill out and submit.
         </label>
         <div className="border border-gray-700 rounded-lg overflow-hidden shadow-lg">
           <Editor
@@ -96,7 +98,8 @@ function RuntimeForm({
 
       <div className="space-y-2">
         <label className="block text-sm">
-          Write a solution to your problem.
+          Write a solution function to your problem. This is the function that will be
+          compared against the user's template function.
         </label>
         <div className="border border-gray-700 rounded-lg overflow-hidden shadow-lg">
           <Editor
@@ -121,8 +124,8 @@ function RuntimeForm({
         <label className="block text-sm">
           Finally, write a function that tests the inputs you generated with the
           user's template function vs your solution. If it passes all test
-          cases, this should only output "all" to stdout. If it fails the first
-          test, it should print 1, if it fails the second test, etc.
+          cases, this should only output "all" to stdout. If it fails, then any
+          output you make will be shown to the user.
         </label>
 
         <div className="border border-gray-700 rounded-lg overflow-hidden shadow-lg">
@@ -135,11 +138,15 @@ function RuntimeForm({
   for (int i = 0; i < testCases.size(); i++) {
     int n = testCases[i];
     if (isNOdd(n) != solution(n)) { 
-      std::cout << i + 1; 
+      std::cout << n << " is wrong"; 
       return;
     }
   }
   std::cout << "all";
+}
+int main() {
+  testing();
+  return 0;
 }`}
             options={{
               fontSize: 14,
